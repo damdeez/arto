563f733856898678b9eb5c7170cee88beeed941f
damirharambasic-tzero <dharambasic-git@tzero.com>
2023-05-03 22:22:13 -0600

    arto the dood, a beginning

---
+++ components/Main.tsx
@@ -0,0 +1,15 @@
+import styles from '../styles/Home.module.css';
+import homeStyles from './Main.module.css';
+import ArtoPic from '../assets/arto-on-porch.jpg';
+import Image from 'next/image';
+
+function Main() {
+  return (
+    <main className={styles.main}>
+      Arto, a dog.
+      <Image src={ArtoPic} alt="arto" className={homeStyles.artoPic} />
+    </main>
+  );
+};
+
+export default Main;
