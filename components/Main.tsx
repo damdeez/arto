9f0a4937dede58b870be19082a310002db4b8a0d
damdeez <damir.hara@gmail.com>
2023-08-09 23:40:42 -0600

    updated packages and api

--- components/Main.tsx
+++ components/Main.tsx
@@ -1,9 +1,18 @@
+import { useEffect } from 'react';
 import styles from '../styles/Home.module.css';
 import homeStyles from './Main.module.css';
 import ArtoPic from '../assets/arto-on-porch.jpg';
 import Image from 'next/image'
 
 function Main() {
+  useEffect(() => {
+    fetch('/api/getStatus')
+      .then((res) => res.json())
+      .then((data) => {
+        console.info('>>> Arto data', data);
+      })
+  }, []);
+
   return (
     <main className={styles.main}>
       Arto, a dog.
