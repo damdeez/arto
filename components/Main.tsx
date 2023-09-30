9dd652f5efeb9c0c51f67ffaab3770a5d0ce379c
damdeez <damir.hara@gmail.com>
2023-09-30 00:37:05 -0600

    style fixes

--- components/Main.tsx
+++ components/Main.tsx
@@ -6,16 +6,15 @@ import ArtoPic from '../assets/arto-on-porch.jpg';
 import Image from 'next/image'
 
 function Main() {
-  const { data, error, isLoading } = useSWR(
+  const { data, isLoading } = useSWR(
     '/api/status',
     (...args) => fetch(...args).then(res => res.json()
     ));
-  console.info('>>> Arto data', data);
 
   return (
     <main className={styles.main}>
       Arto, a dog.
-      <div>I am currently {!isLoading ? data.status : null}</div>
+      <div>I am currently {!isLoading ? data.status : '...'}.</div>
       <Image src={ArtoPic} alt="arto" className={homeStyles.artoPic} />
     </main>
   )
