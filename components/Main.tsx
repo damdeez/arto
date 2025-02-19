f1a56e536d35759bf71ab7d3676e420336a81506
damdeez <damir.hara@gmail.com>
2025-02-18 22:16:45 -0700

    tailwind and such

--- components/Main.tsx
+++ components/Main.tsx
@@ -17,12 +17,12 @@ function Main() {
     <main className={styles.main}>
       Arto, a dog. The current update:
       {summary && (
-        <div className="w-xl mt-4 p-4 bg-slate-100 rounded-lg">
+        <div className="w-xl mt-4 p-4 bg-slate-100 rounded-lg border-1 border-slate-300 shadow-sm">
           {/* <h3 className="font-medium mb-2">Analysis:</h3> */}
           <p>{summary}</p>
         </div>
       )}
-      <Image src={ArtoPic} alt="arto" className={homeStyles.artoPic} />
+      <Image src={ArtoPic} alt="arto" className="w-100 shadow-xl rounded-lg absolute bottom-0" />
     </main>
   );
 }
