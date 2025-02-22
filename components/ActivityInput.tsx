31d878651cc46cf2f4e7a756c5b53c51d64bdc1a
damdeez <damir.hara@gmail.com>
2025-02-22 15:55:56 -0700

    styling updates

--- components/ActivityInput.tsx
+++ components/ActivityInput.tsx
@@ -87,18 +87,18 @@ function ActivityInput() {
   };
 
   return (
-    <main className="flex flex-col gap-2 p-8 sm:flex-row sm:items-center sm:gap-6 sm:py-4">
+    <main className="flex flex-col gap-2 p-8 sm:items-center sm:gap-6 sm:py-4">
       {errorMessage && <div>{errorMessage}</div>}
       <form
         onSubmit={handleSubmit}
-        className="flex-row gap-4 sm:flex sm:flex-col"
+        className="flex flex-col gap-4 sm:flex-col"
       >
         <label htmlFor="activity" className="mb-2 block text-sm font-medium">
           What is Arto doing right now?
         </label>
         <div className="flex flex-col gap-4">
           <textarea
-            className="flex h-100 p-4 border-gray-500 rounded-lg border-2 border-slate-300 shadow-xs text-3xl text-wrap focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50"
+            className="flex h-100 p-4 rounded-lg border-2 border-slate-300 shadow-xs text-3xl text-wrap focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50"
             id="activity"
             value={dogActivity}
             onChange={(e) => setDogActivity(e.target.value)}
@@ -116,7 +116,7 @@ function ActivityInput() {
       {weather && (
         <div className="mt-6 p-4 bg-slate-50 rounded-lg space-y-2">
           <div className="flex items-center justify-between">
-            <div className="flex items-center space-x-2">
+            <div className="flex items-center space-x-2 mr-2">
               {/* <Thermometer className="h-5 w-5" /> */}
               <span>{weather.temperature}°F</span>
             </div>
--- components/Summary.tsx
+++ components/Summary.tsx
@@ -1,7 +1,7 @@
 function Summary({ summary}: { summary: string | null }) {
   return (
     summary && (
-      <div className="w-100% m-4 p-4 mt-8 bg-slate-50 rounded-lg border-1 border-slate-300">
+      <div className="max-w-sm w-100% p-4 mt-8 bg-slate-50 rounded-lg border-1 border-slate-300">
         <h3 className="font-medium mb-2">The current Arto update:</h3>
         <p>{summary}</p>
       </div>
