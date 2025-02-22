b6219e7da3ee5a6133c4242b376443aff0477e32
damdeez <damir.hara@gmail.com>
2025-02-22 15:56:26 -0700

    styling updates

--- components/Summary.tsx
+++ components/Summary.tsx
@@ -1,4 +1,4 @@
-function Summary({ summary}: { summary: string | null }) {
+function Summary({ summary }: { summary: string | null }) {
   return (
     summary && (
       <div className="max-w-sm w-100% p-4 mt-8 bg-slate-50 rounded-lg border-1 border-slate-300">
@@ -7,6 +7,6 @@ function Summary({ summary}: { summary: string | null }) {
       </div>
     )
   );
-};
+}
 
 export default Summary;
